54e6c7738745fc8864353dc017ee2b97a9a9e147
Isaac0759 <kouakou9.kouakou@uvci.edu.ci>
2022-11-08 00:45:30 +0000

    mise a jour

--- assets/JavaScript/script.js
+++ assets/JavaScript/script.js
@@ -1,10 +1,11 @@
 const tache = document.getElementById('task');
 const search = document.getElementById('search');
 const formAddTask = document.querySelector('.add');
-const main = document.querySelector('.content');
+const main = document.querySelector('.list-group');
 
 let tasks = [];
 
+// Au soumission du formulaire
 formAddTask.addEventListener('submit', (e) => {
     e.preventDefault();
     let valeurTache = tache.value;
@@ -18,8 +19,11 @@ formAddTask.addEventListener('submit', (e) => {
     else{
         alert("Vous devez saisir une tache");
     }
+    
 });
 
+
+// Fonction de mise ajout de la page
 const updatePage = () =>{
     const progressBar= document.getElementById('bar');
     document.getElementById('tasks-total').textContent = tasks.length;
@@ -28,40 +32,50 @@ const updatePage = () =>{
 
     const percentage = (completed() * 100 / tasks.length) || 0;
     progressBar.style.width = `${percentage}%`;
-}
+    supprimer();
 
-
-const generateTemplate = () => {
-    const html = `
-        <div class="card mb-1">
-            <div class="card-body d-flex">
-                <div class="input-group">
-                    <input type="checkbox" class="me-2">
-                    <b>${tache.value}</b>
-                </div>
-                <div class="btn-group">
-                    <button class="btn btn-warning"><i class="bi bi-pen"></i></button>
-                    <button class="btn btn-danger delete"><i class="bi bi-trash"></i></button>
-                </div>
-            </div>
-        </div>   
-    `;
-    main.innerHTML += html;
-}
-
-const completed = () => {
-    let nombre = 0;
-    document.querySelectorAll('input[type="checkbox"]').forEach((checkbox) => {
-        if (checkbox.checked) {
-            nombre++;
-            console.log(nombre);
-        }
-    });
-    return nombre;
 };
 
-main.addEventListener('click', (e) =>{
-    if (e.target.classList.contains('delete')) {
-        e.target.parentElement.remove();
-    }
-});
+//MAJ
+
+const maj = () =>{
+    document.querySelectorAll('input[type="checkbox"]').forEach((check)=>{
+        check.addEventListener('click',(e)=>{
+            updatePage();
+        })
+    })
+}
+
+// Fonction qui genere les taches sur le dom
+const generateTemplate = () => {
+    const html = `
+            <label class="list-group-item d-flex gap-3">
+              <input class="form-check-input flex-shrink-0" type="checkbox" style="font-size: 1.375em;">
+              <span class="pt-1 form-checked-content">
+                <strong>${tache.value}</strong>
+              </span>
+              <i class="ms-auto btn btn-danger delete bi bi-trash"></i>
+            </label>
+    `;
+    main.innerHTML += html;
+    maj();
+}
+
+
+//Fonction qui determine le nombre de tache terminé
+const completed = () => {
+    var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked')
+    return checkboxes.length;
+};
+
+//Fonction qui permet de supprimer une tache
+const supprimer = ()=>{
+    main.addEventListener('click', (e) =>{
+        if (e.target.classList.contains('bi-trash')) {
+            e.target.parentElement.remove();
+        }
+    });
+
+}
+
+//Fonction Recherche
